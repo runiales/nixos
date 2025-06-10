34075f093ea0f9243621f7fcca3d3d429d21b210
Cipriano <irpic.am@gmail.com>
2025-06-10 21:24:11 +0200

    firefox

--- configuration.nix
+++ configuration.nix
@@ -4,6 +4,16 @@
 
 { config, pkgs, ... }:
 
+let # se utiliza en la config de firefox
+    lock-false = {
+      Value = false;
+      Status = "locked";
+    };
+    lock-true = {
+      Value = true;
+      Status = "locked";
+    };
+  in
 {
   imports =
     [ # Include the results of the hardware scan.
@@ -185,6 +195,7 @@ dwmblocks = prev.dwmblocks.overrideAttrs (old: {
   htop-vim
   tree
   pstree
+  wget
   ];
 
   programs.gnupg.agent = {
@@ -233,6 +244,76 @@ programs.zsh = {
   autosuggestions.enable = true;
   syntaxHighlighting.enable = true;
 };
+
+  programs.firefox = {
+      enable = true;
+
+      /* ---- POLICIES ---- */
+      # Check about:policies#documentation for options.
+      policies = {
+        DisableTelemetry = true;
+        DisableFirefoxStudies = true;
+	EnableTrackingProtection = {
+          Value= true;
+          Locked = true;
+          Cryptomining = true;
+          Fingerprinting = true;
+        };
+        DisablePocket = true;
+        DisableFirefoxAccounts = true;
+        DisableAccounts = true;
+        DisableFirefoxScreenshots = true;
+        OverrideFirstRunPage = "";
+        OverridePostUpdatePage = "";
+        DontCheckDefaultBrowser = true;
+        DisplayBookmarksToolbar = "never";
+        DisplayMenuBar = "default-off";
+        SearchBar = "unified";
+        NewTabPage = false;
+
+        /* ---- EXTENSIONS ---- */
+        # Check about:support for extension/add-on ID strings.
+        # Valid strings for installation_mode are "allowed", "blocked",
+        # "force_installed" and "normal_installed".
+        ExtensionSettings = {
+          "*".installation_mode = "blocked"; # blocks all addons except the ones specified below
+          # uBlock Origin:
+          "uBlock0@raymondhill.net" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+            installation_mode = "force_installed";
+          };
+          # vimium
+          "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/vimium-ff/latest.xpi";
+            installation_mode = "force_installed";
+          };
+        };
+
+        /* ---- PREFERENCES ---- */
+        # Check about:config for options.
+        Preferences = {
+          "browser.contentblocking.category" = { Value = "strict"; Status = "locked"; };
+          "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
+          "extensions.pocket.enabled" = lock-false;
+          "extensions.screenshots.disabled" = lock-true;
+          "browser.topsites.contile.enabled" = lock-false;
+          "browser.formfill.enable" = lock-false;
+          "browser.search.suggest.enabled" = lock-false;
+          "browser.search.suggest.enabled.private" = lock-false;
+          "browser.urlbar.suggest.searches" = lock-false;
+          "browser.urlbar.showSearchSuggestionsFirst" = lock-false;
+          "browser.newtabpage.activity-stream.feeds.section.topstories" = lock-false;
+          "browser.newtabpage.activity-stream.feeds.snippets" = lock-false;
+          "browser.newtabpage.activity-stream.section.highlights.includePocket" = lock-false;
+          "browser.newtabpage.activity-stream.section.highlights.includeBookmarks" = lock-false;
+          "browser.newtabpage.activity-stream.section.highlights.includeDownloads" = lock-false;
+          "browser.newtabpage.activity-stream.section.highlights.includeVisited" = lock-false;
+          "browser.newtabpage.activity-stream.showSponsored" = lock-false;
+          "browser.newtabpage.activity-stream.system.showSponsored" = lock-false;
+          "browser.newtabpage.activity-stream.showSponsoredTopSites" = lock-false;
+        };
+      };
+    };
 
 nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
