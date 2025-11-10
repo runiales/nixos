c1f918cad377d188b884625ae266b62a254fea79
runiales <irpic.am@gmail.com>
2025-11-10 17:41:54 +0100

    中文

--- configuration.nix
+++ configuration.nix
@@ -432,9 +432,16 @@ Add = [
    i18n.inputMethod = {
      type = "fcitx5";
      enable = true;
-     fcitx5.addons = with pkgs; [
-       fcitx5-chinese-addons
-     ];
+     fcitx5 = {
+       addons = with pkgs; [
+         fcitx5-chinese-addons
+       ];
+       settings = {
+         globalOptions = {
+	   "Hotkey/TriggerKeys"."0" = "Super+space";
+         };
+       };
+     };
    };
 
 #----=[ Fonts ]=----#
